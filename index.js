a05e30e0d765a1955c05a20c405f450787c33dd7
caret_ <dandradepac@outlook.com>
2024-10-29 14:35:40 +0000

    Add comments

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const { setup } = require("./lib/clip.js");
 const { setTimeout } = require("timers/promises");
 
+// get Clip object
 let clip = setup();
 
+// New test
 clip.assert("example", async (t) => {
   await setTimeout(1000);
   t.log("Running example test!");
   return false;
 });
 
+// New test
 clip.assert("another", (t) => {
   t.log("Running another test!");
   return true;
 });
 
+// Get test results
 clip.check();
