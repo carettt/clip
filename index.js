dcd939dea1573b575a904766d24d2915a3a3c4dd
caret_ <dandradepac@outlook.com>
2024-11-04 14:41:00 +0000

    Add basic error propagation

--- index.js
+++ index.js
@@ -9,19 +9,26 @@ let mock = new MockServer();
 
 mock.get("/", "testing");
 mock.start().then(() => {
-  // New test
-  clip.assert("example", async (t) => {
-    await setTimeout(1000);
-    t.log("This test should pass");
+  // Passing test
+  clip.assert("passing", (t) => {
+    t.log("This test should pass after 1 second");
     return true;
   });
 
-  // New test
-  clip.assert("another", async (t) => {
+  // Passing test with fetch from server
+  clip.assert("server", async (t) => {
+    t.log("Fetching from server endpoint /");
     let res = await fetch("http://localhost:3000/");
+    t.log("Parsing response");
     let text = await res.text();
 
-    return text === "tested";
+    return text === "testing";
+  });
+
+  // Failing test with TypeError
+  clip.assert("type-error", () => {
+    ""();
+    return true;
   });
 
   // Get test results
